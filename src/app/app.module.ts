f39377de4a3a0f50d34dcc8d3fed312aa6acd789
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-26 14:16:26 +0200

    ng generate component Detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
