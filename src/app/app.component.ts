55aa906c906f313dca5940f6599cf533694fc503
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-26 11:20:59 +0200

    Display contact infos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './model/contact';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Carnet d\'adresses';
+export class AppComponent implements OnInit {
+  contact = new Contact();
+
+  ngOnInit(): void {
+    this.contact.firstName = 'Tony';
+    this.contact.lastName = 'Stark';
+    this.contact.email = 'tony@stark.com';
+  }
 }
---
+++ src/app/model/contact.ts
@@ -0,0 +1,5 @@
+export class Contact {
+  lastName = '';
+  firstName = '';
+  email = '';
+}
