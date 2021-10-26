2cba3a0004183c027c51e23f9b97728ecd5e15a2
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-26 14:28:32 +0200

    property binding to sub-component

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {Contact} from "../model/contact";
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DetailComponent {
+  @Input()
+  contact?: Contact;
 }
