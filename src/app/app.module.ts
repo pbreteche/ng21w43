bfcb6b3e894d23efc932036533807e3a766a6ad4
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-27 16:46:16 +0200

    Reactive form display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { DetailComponent } from './detail/detail.component';
 import { EmailEditComponent } from './email-edit/email-edit.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { CreateTemplateComponent } from './form/create-template/create-template.component';
+import { CreateReactiveComponent } from './form/create-reactive/create-reactive.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +16,16 @@ import { CreateTemplateComponent } from './form/create-template/create-template.
     DetailComponent,
     EmailEditComponent,
     ContactListComponent,
-    CreateTemplateComponent
+    CreateTemplateComponent,
+    CreateReactiveComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ContactListComponent },
-      { path: 'new', component: CreateTemplateComponent },
+      { path: 'new', component: CreateReactiveComponent },
       { path: ':id', component: DetailComponent },
     ])
   ],
---
+++ src/app/form/create-reactive/create-reactive.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Contact} from "../../model/contact";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-create-reactive',
+  templateUrl: './create-reactive.component.html',
+  styleUrls: ['./create-reactive.component.scss']
+})
+export class CreateReactiveComponent implements OnInit {
+
+  contact = new Contact();
+  form = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', [Validators.required, Validators.pattern(/[A-Za-z0-9]+/)]),
+    email: new FormControl('', Validators.pattern(/[^@]+@[^@]+\.[a-z]{2,}/)),
+  });
+
+  ngOnInit(): void {
+
+  }
+
+}
