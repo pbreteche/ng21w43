e319454e7f533d6238de644c4ad79f25acd36317
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-27 09:04:59 +0200

    subscribe to route param in component

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Contact} from "../model/contact";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {ContactLoader} from "../loader/contact_loader";
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {
-  @Input()
+export class DetailComponent implements OnInit{
   contact?: Contact;
+  loader = new ContactLoader();
+  id: any = null;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) =>
+      this.id = params.get('id')
+    )
+  }
 }
