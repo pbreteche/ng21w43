69e3adddba5b680fa86c9b6441378f5e49f8ee84
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-27 17:23:17 +0200

    submit reactive form

--- src/app/form/create-reactive/create-reactive.component.ts
+++ src/app/form/create-reactive/create-reactive.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {Contact} from "../../model/contact";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ContactLoader} from "../../loader/contact_loader";
 
 @Component({
   selector: 'app-create-reactive',
   templateUrl: './create-reactive.component.html',
   styleUrls: ['./create-reactive.component.scss']
 })
-export class CreateReactiveComponent implements OnInit {
-
-  contact = new Contact();
+export class CreateReactiveComponent {
   form = new FormGroup({
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', [
@@ -21,8 +20,11 @@ export class CreateReactiveComponent implements OnInit {
     ]),
   });
 
-  ngOnInit(): void {
-
+  constructor(private contactLoader: ContactLoader) {
   }
 
+  save() {
+    this.contactLoader.save(Object.assign(new Contact(), this.form.getRawValue()));
+    this.form.reset();
+  }
 }
